b7318e17447b08d1eb85989b5a34495f34bfaea1
Simon Arame <simonarame@hotmail.com>
2016-08-12 02:43:05 -0400

    The use of constant SW_RESTORE allows the window friendly size to be sticky

--- Enlarger/Program.cs
+++ Enlarger/Program.cs
@@ -2,23 +2,24 @@
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 
-namespace ConsoleApplication1
+namespace IdeaResizer
 {
-    class Program
+    static class Program
     {
 
         // Pinvoke declaration for ShowWindow
         private const int SW_SHOWMAXIMIZED = 3;
-
+        private const int SW_RESTORE = 9;
         private const uint SWP_SHOWWINDOW = 0x0040;
 
         [DllImport("user32.dll")]
         static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
         [DllImport("user32.dll")]
-        static extern bool SetWindowPos(IntPtr hWnd, int hWndInsertAfter,int X,int Y,int cx,int cy,uint uFlags);
+        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter,int X,int Y,int cx,int cy,uint uFlags);
 
-        static void Main(string[] args)
+        [STAThread]
+        static void Main()
         {
 
         Process[] processlist = Process.GetProcesses();
@@ -29,14 +30,10 @@ namespace ConsoleApplication1
                 {
                     if(process.ProcessName.Contains("idea"))
                     {
-                        //Console.WriteLine("Process: {0} \r\nID: {1} \r\nWindow title: {2}", process.ProcessName, process.Id, process.MainWindowTitle);
                         var handle = process.MainWindowHandle;
 
-                        //Rectangle monitor = Screen.AllScreens[1].WorkingArea;
-                        //SetWindowPos(handle, 0, monitor.Left, monitor.Top, monitor.Width-200, monitor.Height, SWP_SHOWWINDOW);
-
-                        SetWindowPos(handle, 0, 0, 0, 640*3/2, 480*3/2, SWP_SHOWWINDOW);
-
+                        SetWindowPos(handle, (IntPtr) 0, 0, 0, 640*3/2, 480*3/2, SWP_SHOWWINDOW);
+                        ShowWindow(handle, SW_RESTORE);
                         ShowWindow(handle, SW_SHOWMAXIMIZED);
 
                     }
